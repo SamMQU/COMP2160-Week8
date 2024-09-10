95a1241102d0de7b31a5025f213bb9d5bf541728
alex-valent <alexander.valent@students.mq.edu.au>
2024-09-10 14:49:38 +1000

    coin and player

--- Comp2160 Week-8/Assets/DevB/Movement.cs
+++ Comp2160 Week-8/Assets/DevB/Movement.cs
@@ -1,35 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     private PlayerMovement actions;
     private InputAction movementAction;
+    private Vector2 movementInput;
 
     void Awake()
     {
-        actions = new PlayerActions();
-        movementAction = actions.driving.movement;
-        movementAction = Movement.PlayerActions.Movement;
+        actions = new PlayerMovement();
+        movementAction = actions.FindAction("Movement", throwIfNotFound: true);
     }
+
     void OnEnable()
     {
         movementAction.Enable();
+        movementAction.performed += OnMovementPerformed;
+        movementAction.canceled += OnMovementCanceled;
     }
+
     void OnDisable()
     {
         movementAction.Disable();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        movementAction.performed -= OnMovementPerformed;
+        movementAction.canceled -= OnMovementCanceled;
     }
 
-    // Update is called once per frame
+    private void OnMovementPerformed(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnMovementCanceled(InputAction.CallbackContext context)
+    {
+        movementInput = Vector2.zero;
+    }
+
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(movementInput.x, movementInput.y, 0);
+        transform.Translate(movement * Time.deltaTime);
     }
 }
